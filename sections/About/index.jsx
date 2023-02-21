c17189046271ec159b177b9ef025b83c3a0e3c35
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-02-21 15:34:50 -0300

    feat(master): about - add the description paragraph and the bold styles to some specific words (just like the design)..

--- sections/About/index.jsx
+++ sections/About/index.jsx
@@ -1,7 +1,22 @@
 'use client';
+import React from 'react';
 const About = () => (
   <section
   >
+    <p className='z-10'>
+      <span className='font-bold text-white mr-3'>Metaverse</span>
+      is a new thing in the future, where you can enjoy the virtual
+      world, live new experiences, create new memories and discover
+      new worlds, because this is the new reality in the
+      <span className='font-bold text-white mx-3'>
+        madness of the metaverse.
+      </span>
+      Using only <span className='font-bold text-white mx-3'>VR</span>
+      devices you can easily explore the metaverse the way you want
+      and turn your dreams into reality. Let's
+      <span className='font-bold text-white mx-3'>explore</span> the
+      madness of the metaverse by scrolling down
+    </p>
   </section>
 );
 

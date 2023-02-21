0f5a405651e327d9ec70ff26b6387754f58752e4
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-02-21 16:13:31 -0300

    style(master): about - add padding class in the section wrapper so it has the correct spacing.

--- sections/About/index.jsx
+++ sections/About/index.jsx
@@ -1,26 +1,15 @@
 'use client';
 import React from 'react';
+import styles from '../../styles';
 const About = () => (
   <section
-    className='font-sans text-center lg:text-[32px] md:text-[24]
-  sm:text-[16] text-[14] lg:leading-[57.6px] md:leading-[40px]
-  sm:leading-[30px] leading-[24.4px] uppercase text-[#C7C7C7]
-  2xl:max-w-[1100px] w-full mx-auto flex flex-col align-center relative'
+    className={`${styles.paddings} lg:text-[32px] md:text-[24]
+      2xl:max-w-[1100px] w-full mx-auto flex flex-col align-center relative
+      sm:text-[16] text-[14] lg:leading-[57.6px] md:leading-[40px]
+      sm:leading-[30px] leading-[24.4px] font-sans text-center uppercase
+      text-[#C7C7C7]
+    `}
   >
-    <p className='z-10'>
-      <span className='font-bold text-white mr-3'>Metaverse</span>
-      is a new thing in the future, where you can enjoy the virtual
-      world, live new experiences, create new memories and discover
-      new worlds, because this is the new reality in the
-      <span className='font-bold text-white mx-3'>
-        madness of the metaverse.
-      </span>
-      Using only <span className='font-bold text-white mx-3'>VR</span>
-      devices you can easily explore the metaverse the way you want
-      and turn your dreams into reality. Let's
-      <span className='font-bold text-white mx-3'>explore</span> the
-      madness of the metaverse by scrolling down
-    </p>
 
     <a href='#explore'>
       <img

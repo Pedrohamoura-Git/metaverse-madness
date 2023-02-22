38667b0e94a032ed9e1cd5f20e8d907cb22a4067
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-02-22 17:46:24 -0300

    feat(master): explore - change tsx to jsx and add wrapper classes.

---
+++ sections/Explore/index.jsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+
+const Explore = () => (
+  <section
+    className={`${styles.paddings} mx-auto flex flex-col align-center 2xl:max-w-[1100px] text-white`}
+  >
+  </section>
+);
+
+export default Explore;

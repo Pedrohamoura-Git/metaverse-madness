ec17f0f6f0ba27d8ce913e0e6065cf523e5efdc0
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-02-22 17:47:20 -0300

    feat(master): explore - add the section title and description with slideIn animation.

--- sections/Explore/index.jsx
+++ sections/Explore/index.jsx
@@ -1,10 +1,28 @@
 'use client';
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Explore = () => (
   <section
     className={`${styles.paddings} mx-auto flex flex-col align-center 2xl:max-w-[1100px] text-white`}
   >
+    <motion.div
+      variants={slideIn('down', 'tween', 0.2, 1)}
+      initial='hidden'
+      whileInView='show'
+      className={`${styles.innerWidth} flex flex-col`}
+    >
+      <TypingText
+        title='| The World'
+        textStyles='uppercase mx-auto'
+      />
+
+      <h2
+        className={`lg:text-[64px] md:text-[32px] sm:text-[28px] text-center leading-[80.64px]`}
+      >
+        Choose the world you want to explore
+      </h2>
+    </motion.div>
   </section>
 );
 

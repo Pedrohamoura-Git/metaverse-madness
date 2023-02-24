70588523243903d9baf762f90b447b2280ee8d2d
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-02-24 16:03:50 -0300

    feat(master): explore - toggle classes when the world is selected.

--- components/ExploreCard/index.jsx
+++ components/ExploreCard/index.jsx
@@ -2,15 +2,32 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const ExploreCard = ({ world }) => (
-  <motion.div className='relative'>
-    <p>{world.id}</p>
+const ExploreCard = ({
+  world,
+  selectedWorld,
+  updateSelectedWorld,
+}) => (
+  <motion.button
+    className='relative'
+    onClick={() => updateSelectedWorld(world.id)}
+  >
     <img
       src={world.imgUrl}
-      className='w-[170px] h-[563px] rounded-[24px] object-cover'
+      className={`${
+        world.id === selectedWorld ? 'w-[370px]' : 'w-[170px]'
+      } h-[563px] rounded-[24px] object-cover duration-500`}
     />
-    <p>{world.title}</p>
-  </motion.div>
+
+    <p
+      className={`${
+        world.id === selectedWorld
+          ? 'bottom-[0%] left-[25%] -translate-x-1/4 -translate-y-2/4'
+          : '-rotate-90 max-w-max bottom-[30%] left-[50%] -translate-x-2/4 -translate-y-2/4'
+      } text-4xl font-bold absolute duration-500`}
+    >
+      {world.title}
+    </p>
+  </motion.button>
 );
 
 export default ExploreCard;

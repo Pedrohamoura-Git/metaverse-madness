582c2d0e23cb7606f27620be49db53e9723cd1ec
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-03-03 11:57:19 -0300

    fix(master): getStarted - import framer-motion and custom animations in order for the motion's tags to work properly.

--- sections/GetStarted/index.jsx
+++ sections/GetStarted/index.jsx
@@ -1,7 +1,9 @@
 'use client';
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 import styles from '../../styles';
+import { staggerContainer, fadeIn } from '../../utils/motion';
 
 import { TitleText, TypingText } from '../../components/CustomTexts';
 

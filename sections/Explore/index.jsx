057e16e6e20504fc85b12e3358a7c7f2186a4a82
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-03-13 20:45:34 -0300

    style(master): explore - grid layot for desktop view.

--- sections/Explore/index.jsx
+++ sections/Explore/index.jsx
@@ -42,7 +42,7 @@ const Explore = () => {
           textStyles='text-center'
         />
 
-        <div className='mt-24 flex min-h-[70vh] flex-col gap-5 sm:min-h-[30vh] md:mt-[50px] md:min-h-full  lg:flex-row'>
+        <div className='mt-24 flex min-h-[70vh] flex-col gap-5 sm:min-h-[30vh] md:mt-[50px] md:min-h-full lg:grid lg:grid-flow-col lg:grid-cols-4 lg:grid-rows-2 lg:flex-row'>
           {exploreWorlds.map((world, index) => (
             <ExploreCard
               {...world}

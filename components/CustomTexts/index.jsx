a63c45b9680da0ac0bf00c1c082d1e0197fd82d0
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-03-13 21:52:01 -0300

    style(master): customText - increase titleText margin-bottom to improve fidelity.

--- components/CustomTexts/index.jsx
+++ components/CustomTexts/index.jsx
@@ -11,7 +11,10 @@ export const TypingText = ({ title, textStyles }) => (
     ${textStyles}`}
   >
     {Array.from(title).map((letter, index) => (
-      <motion.span variants={textVariant2} key={`span-${letter + index}`}>
+      <motion.span
+        variants={textVariant2}
+        key={`span-${letter + index}`}
+      >
         {letter === ' ' ? '\u00A0' : letter}
       </motion.span>
     ))}
@@ -23,7 +26,7 @@ export const TitleText = ({ title, textStyles, slot }) => (
     variants={textVariant2}
     initial='hidden'
     whileInView='show'
-    className={`mt-[8px] text-[40px] font-bold text-white md:text-[64px] ${textStyles}`}
+    className={`mt-[8px] mb-12 text-[40px] font-bold text-white md:text-[64px] ${textStyles}`}
   >
     {title}
     {slot !== undefined && { slot }}

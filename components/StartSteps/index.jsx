84594dc9cd4fbb3e72ee38a10d471d0b86a60dda
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-03-27 04:51:41 -0300

    refactor(main): GetStarted/StartSteps - move the steps list to a dedicated component.

---
+++ components/StartSteps/index.jsx
@@ -0,0 +1,35 @@
+'use client';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import styles from '../../styles';
+import { staggerContainer, fadeIn } from '../../utils/motion';
+
+const StartSteps = ({ stepsList }) => (
+  <motion.ol
+    variants={staggerContainer}
+    initial='hidden'
+    whileInView='show'
+    viewport={{ once: false, amount: 0.25 }}
+    className='flex max-w-4xl flex-col items-center gap-6'
+  >
+    {(stepsList ?? []).map(({ description, id }, index) => (
+      <motion.li
+        variants={fadeIn('left', 'spring', index * 0.5, 0.75)}
+        className='flex items-center gap-4'
+        key={id}
+      >
+        <span
+          className={`${styles.flexCenter} glassmorphism bold min-h-[60px] min-w-[60px] rounded-3xl text-xl`}
+        >
+          {index <= 8 ? `0${index + 1}` : index + 1}
+        </span>
+        <span className='max-w-sm text-lg text-[#B0B0B0]'>
+          {description}
+        </span>
+      </motion.li>
+    ))}
+  </motion.ol>
+);
+
+export default StartSteps;
--- sections/GetStarted/index.jsx
+++ sections/GetStarted/index.jsx
@@ -3,13 +3,9 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
 import styles from '../../styles';
-import {
-  staggerContainer,
-  fadeIn,
-  planetVariants,
-} from '../../utils/motion';
+import { staggerContainer, planetVariants } from '../../utils/motion';
 
-import { TitleText, TypingText } from '../../components/CustomTexts';
+import { TitleText, TypingText, StartSteps } from '../../components';
 
 const GetStarted = () => {
   const [instructionsList] = useState([
@@ -55,37 +51,7 @@ const GetStarted = () => {
 
           <TitleText title='Get started with just a few clicks' />
 
-          <motion.ol
-            variants={staggerContainer}
-            initial='hidden'
-            whileInView='show'
-            viewport={{ once: false, amount: 0.25 }}
-            className='flex max-w-4xl flex-col items-center gap-6'
-          >
-            {(instructionsList ?? []).map(
-              ({ description, id }, index) => (
-                <motion.li
-                  variants={fadeIn(
-                    'left',
-                    'spring',
-                    index * 0.5,
-                    0.75,
-                  )}
-                  className='flex items-center gap-4'
-                  key={id}
-                >
-                  <span
-                    className={`${styles.flexCenter} glassmorphism bold min-h-[60px] min-w-[60px] rounded-3xl text-xl`}
-                  >
-                    {index <= 8 ? `0${index + 1}` : index + 1}
-                  </span>
-                  <span className='max-w-sm text-lg text-[#B0B0B0]'>
-                    {description}
-                  </span>
-                </motion.li>
-              ),
-            )}
-          </motion.ol>
+          <StartSteps stepsList={startingFeatures} />
         </div>
       </motion.div>
     </section>

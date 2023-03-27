38fb1425ee91ada19274cd27f522c2a307080ac9
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-03-27 10:30:26 -0300

    style(main): styles file - change the default exports to named exports to allow a custom class to share its content with another.

--- styles/index.js
+++ styles/index.js
@@ -1,38 +1,37 @@
-const styles = {
-  innerWidth: '2xl:max-w-[1280px] w-full',
-  interWidth: 'lg:w-[80%] w-[100%]',
+// Common - dimensions
+export const innerWidth = '2xl:max-w-[1280px] w-full';
+export const interWidth = 'lg:w-[80%] w-[100%]';
 
-  paddings: 'sm:p-16 xs:p-8 px-6 py-12',
-  yPaddings: 'sm:py-16 xs:py-8 py-12',
-  xPaddings: 'sm:px-16 px-6',
-  topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
-  bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
+// Common - spacing
+export const paddings = 'sm:p-16 xs:p-8 px-6 py-12';
+export const yPaddings = 'sm:py-16 xs:py-8 py-12';
+export const xPaddings = 'sm:px-16 px-6';
+export const topPaddings = 'sm:pt-16 xs:pt-8 pt-12';
+export const bottomPaddings = 'sm:pb-16 xs:pb-8 pb-12';
 
-  flexCenter: 'flex justify-center items-center',
-  flexStart: 'flex justify-start items-start',
-  flexEnd: 'flex justify-end',
-  navPadding: 'pt-[98px]',
+// Common - flex
+export const flexCenter = 'flex justify-center items-center';
+export const flexStart = 'flex justify-start items-start';
+export const flexEnd = 'flex justify-end';
+export const navPadding = 'pt-[98px]';
 
-  // sections
-  section: `${innerWidth} mx-auto text-white z-10 max-w-[90%]`,
+// sections
+export const section = `${innerWidth} mx-auto text-white z-10 max-w-[90%]`;
 
-  // hero section
-  heroHeading:
-    'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
-  heroDText:
-    'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
+// hero section
+export const heroHeading =
+  'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white';
+export const heroDText =
+  'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]';
 
-  //   GetStarted and WhatsNew
-  planetsDimensions:
-    'm-auto h-full w-full md:max-h-[450px] md:max-w-[450px]',
+// GetStarted and WhatsNew
+export const planetsDimensions =
+  'm-auto h-full w-full md:max-h-[450px] md:max-w-[450px]';
 
-  // World
-  worldPeople: 'h-18 w-18 rounded-full',
+// World
+export const worldPeople = 'h-18 w-18 rounded-full';
 
-  // Insights
-  insightList: 'flex flex-col gap-8',
-  insightItem:
-    'm-auto w-[366px] flex flex-col gap-4 items-center justify-center md:flex-row md:w-4/5 md:gap-16',
-};
-
-export default styles;
+// Insights
+export const insightList = 'flex flex-col gap-8';
+export const insightItem =
+  'm-auto w-[366px] flex flex-col gap-4 items-center justify-center md:flex-row md:w-4/5 md:gap-16';

57a70d1ff6ec408b5a04bef5e341b935ae9227db
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-03-27 04:59:29 -0300

    refactor(main): WhatsNew/NewFeatures - move the feature list to a dedicated component.

---
+++ components/NewFeatures/index.jsx
@@ -0,0 +1,35 @@
+'use client';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { staggerContainer, fadeIn } from '../../utils/motion';
+
+const NewFeatures = ({ featuresList }) => (
+  <motion.ol
+    variants={staggerContainer}
+    initial='hidden'
+    whileInView='show'
+    viewport={{ once: false, amount: 0.25 }}
+    className='flex max-w-4xl flex-wrap justify-evenly gap-6 md:justify-around md:gap-4'
+  >
+    {(featuresList ?? []).map(
+      ({ id, title, imgUrl, subtitle }, index) => (
+        <motion.li
+          variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+          className='flex min-w-[210px] flex-1 flex-col items-center sm:max-w-[250px] lg:items-baseline'
+          key={id}
+        >
+          <div className='glassmorphism mb-6 flex h-[70px] w-[70px] items-center justify-center rounded-3xl'>
+            <img src={imgUrl} alt={subtitle} className='w-h-6 h-6' />
+          </div>
+          <h5 className='mb-4 text-2xl font-bold'>{title}</h5>
+          <p className='text-center text-lg text-[#B0B0B0] lg:text-left'>
+            {subtitle}
+          </p>
+        </motion.li>
+      ),
+    )}
+  </motion.ol>
+);
+
+export default NewFeatures;
--- sections/WhatsNew/index.jsx
+++ sections/WhatsNew/index.jsx
@@ -1,15 +1,12 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
-
 import styles from '../../styles';
-import {
-  staggerContainer,
-  fadeIn,
-  planetVariants,
-} from '../../utils/motion';
 
-import { TitleText, TypingText } from '../../components/CustomTexts';
+import { staggerContainer, planetVariants } from '../../utils/motion';
+
+import { TitleText, TypingText, NewFeatures } from '../../components';
+
 
 const GetStarted = () => {
   const [newsList] = useState([
@@ -45,40 +42,7 @@ const GetStarted = () => {
 
           <TitleText title="What's new about Metaversus?" />
 
-          <motion.ol
-            variants={staggerContainer}
-            initial='hidden'
-            whileInView='show'
-            viewport={{ once: false, amount: 0.25 }}
-            className='flex max-w-4xl flex-wrap justify-evenly gap-6 md:justify-around md:gap-4'
-          >
-            {(newsList ?? []).map(
-              ({ id, title, img, description }, index) => (
-                <motion.li
-                  variants={fadeIn(
-                    'right',
-                    'spring',
-                    index * 0.5,
-                    0.75,
-                  )}
-                  className='flex min-w-[210px] flex-1 flex-col items-center sm:max-w-[250px] lg:items-baseline'
-                  key={id}
-                >
-                  <div className='glassmorphism mb-6 flex h-[70px] w-[70px] items-center justify-center rounded-3xl'>
-                    <img
-                      src={img}
-                      alt={description}
-                      className='w-h-6 h-6'
-                    />
-                  </div>
-                  <h5 className='mb-4 text-2xl font-bold'>{title}</h5>
-                  <p className='text-center text-lg text-[#B0B0B0] lg:text-left'>
-                    {description}
-                  </p>
-                </motion.li>
-              ),
-            )}
-          </motion.ol>
+          <NewFeatures featuresList={newFeatures} />
         </div>
 
         <motion.div variants={planetVariants('right')}>

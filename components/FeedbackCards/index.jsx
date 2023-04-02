ef1a87b8d75d8c1a89be5b2fee2b6a624766b895
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-04-01 21:01:52 -0300

    style(main): FeedbackCards - add fadeIn animation.

--- components/FeedbackCards/index.jsx
+++ components/FeedbackCards/index.jsx
@@ -1,9 +1,17 @@
 'use client';
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import { fadeIn } from '../../utils/motion';
 
 const FeedbackCards = ({ feedbacks }) => {
   return (
-    <ul className='relative flex flex-col gap-8'>
+    <motion.ul
+      variants={fadeIn('right', 'tween', 0.75, 0.75)}
+      initial='hidden'
+      whileInView='show'
+      className='relative flex flex-col gap-8 lg:flex-[0.5]'
+    >
       {feedbacks.length &&
         feedbacks.map(({ name, occupation, feedback }, index) => (
           <li key={name + index} className='rounded-3xl border p-4'>
@@ -14,7 +22,7 @@ const FeedbackCards = ({ feedbacks }) => {
             <p className='mt-6 text-lg leading-9'>{feedback}</p>
           </li>
         ))}
-    </ul>
+    </motion.ul>
   );
 };
 

972c3a88b5c495f7c5adea930d8e287f3d57b207
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-04-07 10:29:33 -0300

    feat(main): footer - creates the footer component with mobile, tablet and desktop responsiveness styles.

---
+++ components/Footer/index.jsx
@@ -0,0 +1,69 @@
+'use client';
+
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+import { section, innerWidth, paddings } from '../../styles';
+import { staggerContainer, fadeIn, zoomIn } from '../../utils/motion';
+import { TitleText, MetaverseBtn } from '../index';
+
+const Footer = () => (
+  <footer className={`relative ${section}`}>
+    <div className={`${innerWidth} ${paddings} `}>
+      <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between'>
+        <TitleText
+          title='Enter the Metaverse'
+          textStyles='!text-left mb-5 md:w-max md:p-0 md:mb-5'
+        />
+
+        <MetaverseBtn color='blue' styles='mt-0' />
+      </div>
+
+      <div className='mt-8 mb-12 border-b-2 opacity-20' />
+
+      <div className='flex flex-col gap-4 md:flex-row md:items-center md:justify-between'>
+        <p className='text-2xl font-extrabold uppercase leading-5'>
+          Metaversus
+        </p>
+        <p className='text-sm opacity-50'>
+          Copyright © 2021 - 2022 Metaversus. All rights reserved.
+        </p>
+
+        <div className='flex gap-4'>
+          <a href='#' target='_blank'>
+            <img
+              src='/twitter.svg'
+              alt='twitter-icon'
+              className='h-6 w-6'
+            />
+          </a>
+          <a href='#' target='_blank'>
+            <img
+              src='/linkedin.svg'
+              alt='linkedin-icon'
+              className='h-6 w-6'
+            />
+          </a>
+          <a href='#' target='_blank'>
+            <img
+              src='/instagram.svg'
+              alt='instagram-icon'
+              className='h-6 w-6'
+            />
+          </a>
+          <a href='#' target='_blank'>
+            <img
+              src='/facebook.svg'
+              alt='facebook-icon'
+              className='h-6 w-6'
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className='footer-gradient absolute bottom-0 right-0' />
+    </div>
+  </footer>
+);
+
+export default Footer;

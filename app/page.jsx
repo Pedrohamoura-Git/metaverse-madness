c47dc85df8a99c0bcfef2112d1fdf4e6d1c7b023
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-04-16 20:23:27 -0300

    style(main): app -  add padding between sections to improve UX, specially with the new scrollbar on desktop views.

--- app/page.jsx
+++ app/page.jsx
@@ -11,25 +11,27 @@ import {
   World,
 } from '../sections';
 
+import { yPaddings } from '../styles';
+
 const Page = () => (
-  <div className='overflow-hidden bg-primary-black'>
+  <div className='body-wrapper overflow-hidden bg-primary-black'>
     <Navbar />
     <ScrollBar />
     <Hero />
-    <div className='relative'>
+    <div className={`${yPaddings} relative`}>
       <About />
-      <div className='z-0 gradient-03' />
+      <div className='gradient-03 z-0' />
       <Explore />
     </div>
-    <div className='relative'>
+    <div className={`${yPaddings} relative`}>
       <GetStarted />
-      <div className='z-0 gradient-04' />
+      <div className='gradient-04 z-0' />
       <WhatsNew />
     </div>
     <World />
-    <div className='relative'>
+    <div className={`${yPaddings} relative`}>
       <Insights />
-      <div className='z-0 gradient-04' />
+      <div className='gradient-04 z-0' />
       <Feedback />
     </div>
     <Footer />

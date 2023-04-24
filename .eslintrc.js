2b95e7760e82e440741193f7a009bb93188d1b47
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2023-04-24 10:56:31 -0300

    chore: removes typescript from the project so the first build can be deployed. Typescript will be further added from a new branch.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,13 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
   overrides: [],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react"],
   rules: {},
 };
